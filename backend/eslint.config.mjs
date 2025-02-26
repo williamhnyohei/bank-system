1e738e7e832e9eac6121129e1304cb6472776f1e
williamhnyohei <williamhnyohei@gmail.com>
2025-02-26 17:57:47 -0300

    fix(BANK-7): Corrige configuração de esLint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,35 +1,38 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
+export default [
   {
     ignores: ['eslint.config.mjs'],
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
   {
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    plugins: ['@typescript-eslint', 'prettier'],
     languageOptions: {
       globals: {
-        ...globals.node,
-        ...globals.jest,
+        node: true,
+        jest: true,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 'latest',
       sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+        },
+      ],
     },
   },
-);
+];
