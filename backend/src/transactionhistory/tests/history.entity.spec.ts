ad196ed24d6fead78b30ef9d13c7a6481a241ec4
CaioTCjapa <caiotagimaa@gmail.com>
2025-02-27 18:01:25 -0300

    Revert "feat(BANK-8): CPF validator sem services"
    This reverts commit 61b8256519ac26aac31441329d76727690ad2659.

--- backend/src/transactionhistory/tests/history.entity.spec.ts
+++ backend/src/transactionhistory/tests/history.entity.spec.ts
@@ -1,6 +1,6 @@
 import { TransactionHistory } from '../history.entity';
 import { Account } from '../../accounts/account.entity';
-import { TransactionType } from '../transactiontype.enum';
+import { TransactionType } from '../transaction-type.enum';
 
 describe('TransactionHistory Entity', () => {
   it('deve criar um histórico de transação válido', () => {
