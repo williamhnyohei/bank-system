273f9b61c2a64509e07d7ca8135c01711de61421
williamhnyohei <williamhnyohei@gmail.com>
2025-03-12 22:12:17 -0300

    fix(BANK-41): corrige TESTES

--- backend/src/APPs/app.module.spec.ts
+++ backend/src/APPs/app.module.spec.ts
@@ -3,18 +3,30 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { UserModule } from '../Modules/users/user.module';
 import { UserService } from '../Modules/users/user.service';
-import { expect } from '@jest/globals';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User_bank } from '../Modules/users/user.entity';
 
-describe('UserModule', () => {
+describe('AppModule', () => {
   let app: INestApplication;
   let userService = { findAll: () => ['test'] };
+  let userRepository: Partial<Repository<User_bank>>;
 
   beforeAll(async () => {
+    userRepository = {
+      find: jest.fn().mockResolvedValue([]),
+      findOne: jest.fn().mockResolvedValue(null),
+      save: jest.fn(),
+      delete: jest.fn(),
+    };
+
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [UserModule],
     })
       .overrideProvider(UserService)
       .useValue(userService)
+      .overrideProvider(getRepositoryToken(User_bank)) // Mock do repositório
+      .useValue(userRepository)
       .compile();
 
     app = moduleRef.createNestApplication();
@@ -29,10 +41,8 @@ describe('UserModule', () => {
     return request(app.getHttpServer())
       .get('/users')
       .expect(200)
-      .expect({
-        data: userService.findAll(),
-      });
-  });
+      .expect(userService.findAll());
+  });  
 
   afterAll(async () => {
     await app.close();
